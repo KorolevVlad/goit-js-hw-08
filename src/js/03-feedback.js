b73c2e6489af5e8cdb79d39781a95b491ff681a2
KorolevVlad <korolev.v.s@bk.ru>
2022-01-07 16:33:55 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const formData = document.querySelector('.feedback-form');
 
+formData.addEventListener('submit', onSubmit);
+formData.addEventListener('input', throttle(onInput, 500));
+
+function onSubmit(e) {
+  e.preventDefault();
+  const newEmail = e.currentTarget.elements.email.value.trim();
+  const newMessage = e.currentTarget.elements.message.value.trim();
+  const newFormData = {
+    email: newEmail,
+    message: newMessage,
+  };
+
+  if (!newEmail || !newMessage) return alert('Fill in all fields of the form');
+
+  console.log(newFormData);
+  formData.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(e) {
+  const newEmail = formData.elements.email.value.trim();
+  const newMessage = formData.elements.message.value.trim();
+  const newFormData = {
+    email: newEmail,
+    message: newMessage,
+  };
+  newFormData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(newFormData));
+}
+
+function populateMessageOutput() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    formData.email.value = JSON.parse(savedMessage).email;
+    formData.message.value = JSON.parse(savedMessage).message;
+  }
+}
+populateMessageOutput();
